941bb10c856f71f0fca4f25798b072f03d5a4869
RonyAbreu <ronyelison78@gmail.com>
2024-11-18 12:51:38 -0300

    First: criação do projeto

---
+++ src/main/java/com/ufpb/br/apps4society/my_trace_table_manager/MyTraceTableManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.br.apps4society.my_trace_table_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTraceTableManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTraceTableManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpb/br/apps4society/my_trace_table_manager/MyTraceTableManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpb.br.apps4society.my_trace_table_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTraceTableManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
